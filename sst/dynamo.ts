9e986fbb458f2648f35a988409cabc7e29af9295
Craig R. Hughes <craig.git@rungie.com>
2025-06-24 22:05:47 -0700

    Remove optional unindexed fields from dynamo table definition, but include them as comments

--- sst/dynamo.ts
+++ sst/dynamo.ts
@@ -3,22 +3,28 @@
 const buildingsUnitsTable = new sst.aws.Dynamo('BuildingsUnits', {
     fields: {
         buildingID: 'string',
+        // Will be the unit number for a specific unit, or 'BUILDING' for building-level data
         unitID: 'string',
-        street: 'string',
-        city: 'string',
-        state: 'string',
-        zip: 'string',
-        buildingDescription: 'string',
-        yearBuilt: 'number',
-        numberStories: 'number',
-        totalUnits: 'number',
-        unitDescription: 'string',
-        beds: 'number',
-        baths: 'number',
-        sqft: 'number',
-        rent: 'number',
-        occupied: 'boolean',
-        availableDate: 'string',
+
+        // The fields below are not specified because they aren't indexed, and are optional.
+        // Building details:
+        // street: 'string',
+        // city: 'string',
+        // state: 'string',
+        // zip: 'string',
+        // buildingDescription: 'string',
+        // yearBuilt: 'number',
+        // numberStories: 'number',
+        // totalUnits: 'number',
+
+        // Unit details:
+        // unitDescription: 'string',
+        // beds: 'number',
+        // baths: 'number',
+        // sqft: 'number',
+        // rent: 'number',
+        // occupied: 'boolean',
+        // availableDate: 'string',
     },
     primaryIndex: { hashKey: 'buildingID', rangeKey: 'unitID' },
 });
