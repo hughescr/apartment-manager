c256868e47ab254b81883ebe954cd0ee40acfa21
Craig R. Hughes <craig.git@rungie.com>
2025-06-26 17:47:32 -0700

    Fix API router

--- sst/api.ts
+++ sst/api.ts
@@ -4,20 +4,10 @@ import { router } from './router';
 
 export const api = new sst.aws.Function('Api', {
     handler: 'api/index.handler',
-    url: true,
-    router: {
-        instance: router,
-        routes: {
-            'GET /api/buildings': 'api/index.handler',
-            'POST /api/buildings': 'api/index.handler',
-            'GET /api/buildings/{buildingID}': 'api/index.handler',
-            'PUT /api/buildings/{buildingID}': 'api/index.handler',
-            'DELETE /api/buildings/{buildingID}': 'api/index.handler',
-            'GET /api/buildings/{buildingID}/units': 'api/index.handler',
-            'POST /api/buildings/{buildingID}/units': 'api/index.handler',
-            'GET /api/buildings/{buildingID}/units/{unitID}': 'api/index.handler',
-            'PUT /api/buildings/{buildingID}/units/{unitID}': 'api/index.handler',
-            'DELETE /api/buildings/{buildingID}/units/{unitID}': 'api/index.handler',
+    url: {
+        router: {
+            instance: router,
+            path: '/api',
         }
     },
 });
