ac94ac14a04d03ccfaef8914fb3871af4d002aaa
Craig R. Hughes <craig.git@rungie.com>
2025-06-29 19:17:01 -0700

    Fix DynamoToolbox calls to work right; do not let "put" over-write existing items, fail instead; properly return new item on succesful updates.

--- data/buildings.ts
+++ data/buildings.ts
@@ -11,33 +11,39 @@ export async function getBuildings() {
     const { Items } = await ApartmentTable.build(ScanCommand)
         .entities(Building)
         .send();
-    return Items;
+    return Items as BuildingData[];
 }
 
 export async function getBuilding(buildingID: string) {
     const { Item } = await Building.build(GetItemCommand)
         .key({ buildingID, unitID: 'BUILDING' })
         .send();
-    return Item;
+    return Item as BuildingData;
 }
 
 export async function createBuilding(building: BuildingData) {
     const { Attributes } = await Building.build(PutItemCommand)
         .item({ ...building, unitID: 'BUILDING' })
+        .options({
+            condition: { // Fail if unit already exists
+                attr: 'buildingID', exists: false,
+            },
+            returnValuesOnConditionFalse: 'ALL_OLD',
+        })
         .send();
-    return Attributes;
+    return Attributes || building as BuildingData;
 }
 
 export async function updateBuilding(buildingID: string, updates: Partial<BuildingData>) {
     const { Attributes } = await Building.build(UpdateItemCommand)
         .item({ ...updates, buildingID, unitID: 'BUILDING' })
+        .options({ returnValues: 'ALL_NEW' })
         .send();
-    return Attributes;
+    return Attributes as BuildingData;
 }
 
 export async function deleteBuilding(buildingID: string) {
     await Building.build(DeleteItemCommand)
         .key({ buildingID, unitID: 'BUILDING' })
         .send();
-    return true;
 }
--- data/units.ts
+++ data/units.ts
@@ -7,41 +7,47 @@ import { PutItemCommand } from 'dynamodb-toolbox/entity/actions/put';
 import { UpdateItemCommand } from 'dynamodb-toolbox/entity/actions/update';
 import { DeleteItemCommand } from 'dynamodb-toolbox/entity/actions/delete';
 
-import { logger } from '@hughescr/logger';
-
 export async function getUnits(buildingID: string) {
     const { Items } = await ApartmentTable.build(QueryCommand)
     .entities(Unit)
     .query({ partition: buildingID })
     .send();
-    return Items;
+    return Items as UnitData[];
 }
 
 export async function getUnit(buildingID: string, unitID: string) {
     const { Item } = await Unit.build(GetItemCommand)
         .key({ buildingID, unitID })
         .send();
-    return Item;
+    return Item as UnitData;
 }
 
 export async function createUnit(unit: UnitData) {
-    logger.debug(`Creating unit: ${JSON.stringify(unit)}`);
     const { Attributes } = await Unit.build(PutItemCommand)
         .item(unit)
+        .options({
+            condition: { // Fail if unit already exists
+                and: [
+                    { attr: 'buildingID', exists: false },
+                    { attr: 'unitID', exists: false },
+                ],
+            },
+            returnValuesOnConditionFalse: 'ALL_OLD',
+        })
         .send();
-    return Attributes;
+    return Attributes || unit  as UnitData;
 }
 
 export async function updateUnit(buildingID: string, unitID: string, updates: Partial<UnitData>) {
     const { Attributes } = await Unit.build(UpdateItemCommand)
         .item({ ...updates, buildingID, unitID })
+        .options({ returnValues: 'ALL_NEW' })
         .send();
-    return Attributes;
+    return Attributes as UnitData;
 }
 
 export async function deleteUnit(buildingID: string, unitID: string) {
     await Unit.build(DeleteItemCommand)
         .key({ buildingID, unitID })
         .send();
-    return true;
 }
